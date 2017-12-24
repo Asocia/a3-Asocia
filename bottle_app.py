f826315c47f23b1180054f3076e0ef76371349e5
Asocia <akkayas17@itu.edu.tr>
2017-12-24 18:34:45 +0200

    Add filter page.

--- bottle_app.py
+++ bottle_app.py
@@ -13,18 +13,18 @@ content1=[]
 letters="ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz9876543210-' "
 trans={i:letters.index(i) for i in letters}
 navbar="""<ul>
-  <li><a class="active" href="/">Home</a></li>
-  <li><a href="/search">Search a Province</a></li>
-  <li><a href="/sort">Sort by...</a></li>
-  <li><a href="/filter">Filter</a></li>
-  <li><a href="#login">Login</a></li>
-</ul>
+		<li><a class="active" href="/">Home</a></li>
+		<li><a href="/search">Search a Province</a></li>
+		<li><a href="/sort">Sort by...</a></li>
+		<li><a href="/filter">Filter</a></li>
+		<li><a href="#login">Login</a></li>
+	</ul>
 <br>"""
 def split_contents():
 	global meta1,content1
 	meta1=a2.contents[:a2.find_starting_point("total",a2.contents)+1]
 	content1=a2.contents[a2.find_starting_point("total",a2.contents)+1:]
-
+split_contents()
 def look_at_this_dude(li):
 	return ([trans.get(li[0][i]) for i in range(len(li[0]))])
 
@@ -47,7 +47,7 @@ def sort_by_what():
 			break
 	if b!="True":
 		di["ao"],di["do"]=di["do"],di["ao"]
-	form="""<form action="/sort" method="GET" >Sort by
+	form="""<form action="/sort"  method="GET" >Sort by
                 <select name="a">
                     <option value="name" %(name)s>Province name</option>
                     <option value="pop" %(pop)s>Total population</option>
@@ -60,7 +60,7 @@ def sort_by_what():
                 <input type="radio" name="b" value="False" %(do)s>descending order.
                 <input type="submit" value="Apply" />
              </form><br>"""%di
-	split_contents()
+	
 	global meta1, content1
 	meta,content=deepcopy(meta1),deepcopy(content1)
 	data=[]
@@ -104,8 +104,9 @@ def sort_by_what():
 	data=meta+data
 	return a2.template %{'title':"Sorted by %s in %s Order "%(a,b),'navbar':navbar,'form':form,'table':a2.make_table(data)}
 def search_province():
+	global content1,meta1
+	meta,content=deepcopy(meta1),deepcopy(content1)
 	form="""<form action="/search" method="GET" class="simple-form" >
-	
                 <input size="45" type="search" placeholder="Please enter at least 2 characters. (Only in English) " pattern="[A-Za-z]{2,}" required name="province">
                 <input type="submit" value="Search" />
              </form><br>"""
@@ -115,9 +116,7 @@ def search_province():
 		name=""
 	if name =="":
 		return a2.template %{'title':"Search a Province",'navbar':navbar,'table':a2.make_table(a2.contents),'form':form}
-	split_contents()
-	global content1,meta1
-	meta,content=deepcopy(meta1)[:a2.find_starting_point("total",a2.contents)+1],deepcopy(content1)
+
 	data=[]
 	for row in content:
 		if name.lower() in row[0].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u'):
@@ -133,87 +132,100 @@ def search_province():
 
 def get_filter_criterias():
 	form="""<form action="/set+filter+conditions" method="POST" >Please select filtering criteria(s):<br>
-                
-                    <input type="checkbox" name="name" value="name">Province name<br>
-                    <input type="checkbox" name="pop" value="pop">Total population<br>
-                    <input type="checkbox" name="in" value="in">In-migration<br>
-                    <input type="checkbox" name="out" value="out">Out-migration<br>
-                    <input type="checkbox" name="net" value="net">Net migration<br>
-                    <input type="checkbox" name="rate" value="rate">Rate of net migration<br>
-                    <input type="submit" value="Apply" />
-             </form><br>"""
+		<input type="checkbox" name="name" value="name">Province name<br>
+		<input type="checkbox" name="pop" value="pop">Total population<br>
+		<input type="checkbox" name="in" value="in">In-migration<br>
+		<input type="checkbox" name="out" value="out">Out-migration<br>
+		<input type="checkbox" name="net" value="net">Net migration<br>
+		<input type="checkbox" name="rate" value="rate">Rate of net migration<br>
+		<input type="submit" value="Apply" />
+	</form><br>"""
 	return a2.template %{'title':"Filter Criteria...",'navbar':navbar,'form':form,'table':a2.make_table(a2.contents)}
 
 def get_filter_cond():
-	return 'Şimdi uykum var, yarın gel söylicem.'
-	#~ """
-	#~ <form action="/filterasd" method="POST">Please specify filter condition(s):<br>
-	#~ %(name)s <br>
-	#~ %(pop)s <br>
-	#~ %(in)s <br>
-	#~ %(out)s <br>
-	#~ %(net)s <br>
-	#~ %(rate)s <br>
-	#~ """
-	#~ di={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
-	#~ for i in di.keys():
-		#~ if type(request.POST.get(i)) is str:
-			#~ di[i]="+"
+	cond="""
+	<form action="/filterresult" class="simple-form" method="POST">
+		<!--<p>Please specify filter condition(s):</p>-->
+	%(name)s%(pop)s%(in)s%(out)s %(net)s%(rate)s
+		<input type="submit" value="Filter">
+	</form>
+	"""
+	di={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
+	for i in di.keys():
+		if type(request.POST.get(i)) is str:
+			di[i]="+"
+	form_input="""<div class="inputs">
+			<label>%(ln)s</label>
+			<select name="%(name)sgl">
+				<option value="g">greater than</option>
+				<option value="l">less than</option>
+			</select>
+			<input type="number" min="0" name="%(name)s" value="0" step="1" required>
+			<br>
+		</div>"""
+	ln={'pop':"Population",'in':"In-migration",'out':"Out-migration",'net':"Net migration",'rate':"Rate of net migration"}
+	inputs={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
+	for i in di.keys():
+		if di[i]=='+':
+			if i=='name':
+				inputs[i]="""	<div class="inputs">
+			<label>Province name includes</label>
+			<input size="45" type="search"  pattern=".{1,}" name="name" required ><br>
+			<label for="cs">Case-sensitive</label>
+			<input type="checkbox" name="cs" value="enabled">
+			<br>
+		</div>"""
+			else:
+				inputs[i]=form_input%{'ln':ln[i],'name':i}
+	return a2.template %{'title':"Set filter condition(s)",'navbar':navbar,'table':a2.make_table(a2.contents),'form':cond%inputs}
+def filt_name(search,cs,data):
+	if cs=='enabled':
+		new_data=[row for row in data if search in row[0]]
+	else:
+		search=search.replace('İ','i').lower()
+		new_data=[row for row in data if search in row[0].replace('İ','i').lower()]
+	return new_data
 	
-			
-	#~ if type(request.POST.get("name")) is str:
-		#~ f_listrequest.POST.get("name")
-	
-	#~ name=request.POST.get("name")
-	#~ pop=request.POST.get("pop")
-	#~ inm=request.POST.get("in")
-	#~ out=request.POST.get("out")
-	#~ net=request.POST.get("net")
-	
-		#~ print(type(request.POST.get("name")))
-	
-	
-	#~ return name+pop+inm+out+net
+def filter_it(col,val,gl,data):
+	if gl=='g':
+		new_data=[row for row in data if row[a2.find_starting_point(col,a2.contents)]>int(val)]
+	elif gl=='l':
+		new_data=[row for row in data if row[a2.find_starting_point(col,a2.contents)]<int(val)]
+	return new_data
+
+def filter_result():
+	global meta1,content1
+	meta,content=deepcopy(meta1),deepcopy(content1)
+	data=[]
+	data+=content
+	di={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
+	for i in di.keys():
+		if type(request.POST.get(i)) is str:
+			di[i]="+"
+	for i in di.keys():
+		if di[i]=='+':
+			if i=='name':
+				search=request.POST.get(i)
+				cs=request.POST.get('cs')
+				data=filt_name(search,cs,data)
+			else:
+				col=i
+				val=request.POST.get(i)
+				gl=request.POST.get(i+"gl")
+				data=filter_it(col,val,gl,data)
+	if len(data)==0:
+		return a2.template %{'title':"Filter Result",'navbar':navbar,'form':"",'table':"<p>No records were found that match the specified search crieteria :( <a href=\"/search\"> See full table...</a></p>"}
+	data=meta+data
+	return a2.template %{'title':"Filter Result",'navbar':navbar,'form':"",'table':a2.make_table(data)}
+
 def server_static(path):
 	return static_file(path, root=".")
-
-
-
-
-
-
-
-#~ def test():
-	#~ html="""<form action='/biseyler' method='POST'>
-	#~ <input name="name" type='text'>
-	#~ <input type='submit' value='submit'>
-	#~ </form>
-	#~ """
-	#~ return html
-#~ def myfunc():
-	
-	#~ print(len(request.POST))
-	#~ return 'oha oldu laaaaannnn!'
-#~ route('/test','GET', test)
-#~ route('/biseyler','POST', myfunc)
-
-
-
-
-
-
-
-
-
-
-
-
-
 route('/search', 'GET', search_province)
 route('/', 'GET', index)
 route('/sort', 'GET', sort_by_what)
 route('/filter', 'GET', get_filter_criterias)
 route('/set+filter+conditions', 'POST', get_filter_cond)
+route('/filterresult', 'POST', filter_result)
 route('/static/<path>', 'GET', server_static)
 
 
