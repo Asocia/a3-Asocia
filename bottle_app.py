e0a2c5d82820e0a4435be39cf0d5c9285ff8853b
Asocia <akkayas17@itu.edu.tr>
2017-12-25 02:36:48 +0200

    Fix some bugs.

--- bottle_app.py
+++ bottle_app.py
@@ -1,23 +1,25 @@
-
 #####################################################################
 ### Assignment skeleton
 ### You can alter the below code to make your own dynamic website.
 ### The landing page for assignment 3 should be at /
 #####################################################################
-
 from bottle import route, run, default_app, debug, static_file, request
 from copy import deepcopy
 import a2
 meta1=[]
 content1=[]
 letters="ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz9876543210-' "
+tr="ÇĞİÖŞÜçğıöşü"
+en="CGIOSUcgiosu"
+col_names={"name":"Province name","pop":"Population","in":"In-migration","out":"Out-migration","rate":"Rate of net migration","net":"Net migration"}
+tr_to_en={tr[i]:en[i] for i in range(len(tr))}
 trans={i:letters.index(i) for i in letters}
 navbar="""<ul>
 		<li><a class="active" href="/">Home</a></li>
 		<li><a href="/search">Search a Province</a></li>
 		<li><a href="/sort">Sort by...</a></li>
 		<li><a href="/filter">Filter</a></li>
-		<li><a href="#login">Login</a></li>
+		<li><a href="#">Login...</a></li>
 	</ul>
 <br>"""
 def split_contents():
@@ -60,7 +62,6 @@ def sort_by_what():
                 <input type="radio" name="b" value="False" %(do)s>descending order.
                 <input type="submit" value="Apply" />
              </form><br>"""%di
-	
 	global meta1, content1
 	meta,content=deepcopy(meta1),deepcopy(content1)
 	data=[]
@@ -80,7 +81,6 @@ def sort_by_what():
 			meta[a2.find_starting_point("total",a2.contents)-1][0]="( &nabla; ) " +meta[a2.find_starting_point("total",a2.contents)-1][0]
 			b="Descending"
 			data+=reversed(sorted(content,key=look_at_this_dude))
-		a="Province Name"
 	else:
 		content.sort(key = lambda x : x[a2.find_starting_point(a,a2.contents)])
 		if b=="True":
@@ -91,23 +91,14 @@ def sort_by_what():
 			meta[a2.find_starting_point("total",a2.contents)-1][a2.find_starting_point(a,a2.contents)]="( &nabla; ) " + meta[a2.find_starting_point("total",a2.contents)-1][a2.find_starting_point(a,a2.contents)]
 			b="Descending"
 			data+=reversed(content)
-		if a=="pop":
-			a="Population"
-		elif a == "in":
-			a="In-migration"
-		elif a == "out":
-			a="Out-migration"
-		elif a == "net":
-			a="Net migration"
-		else:
-			a="Rate of Net Migration"
+	meta[0][0]="Sorted by %s in %s Order "%(col_names[a],b)
 	data=meta+data
-	return a2.template %{'title':"Sorted by %s in %s Order "%(a,b),'navbar':navbar,'form':form,'table':a2.make_table(data)}
+	return a2.template %{'title':"Sorted by %s"%col_names[a],'navbar':navbar,'form':form,'table':a2.make_table(data)}
 def search_province():
 	global content1,meta1
 	meta,content=deepcopy(meta1),deepcopy(content1)
 	form="""<form action="/search" method="GET" class="simple-form" >
-                <input size="45" type="search" placeholder="Please enter at least 2 characters. (Only in English) " pattern="[A-Za-z]{2,}" required name="province">
+                <input size="55" type="search" placeholder="Please enter at least 2 characters. Only English characters allowed." pattern="[A-Za-z]{2,}" required name="province">
                 <input type="submit" value="Search" />
              </form><br>"""
 	try:
@@ -116,18 +107,27 @@ def search_province():
 		name=""
 	if name =="":
 		return a2.template %{'title':"Search a Province",'navbar':navbar,'table':a2.make_table(a2.contents),'form':form}
-
 	data=[]
 	for row in content:
 		if name.lower() in row[0].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u'):
-			if name.lower()== row[0][:len(name.lower())].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u'):
+			if row[0].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u').startswith(name.lower()):
+				p=row[0].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u').partition(name.lower())
+				l=list(p)
+				l[1]="<b>"+l[1].title()+"</b>"
+				row[0]="".join(l)
 				data=[row]+data
 			else:
+				p=row[0].replace('İ','i').lower().replace('ç','c').replace('ğ','g').replace('ı','i').replace('ö','o').replace('ş','s').replace('ü','u').partition(name.lower())
+				l=list(p)
+				l[0]=l[0].title()
+				l[1]="<b>"+l[1]+"</b>"
+				row[0]="".join(l)
 				data+=[row]
 	data=meta+data
 	if data==meta:
 		return a2.template %{'title':"Searching Result",'navbar':navbar,'form':form,'table':"<p>No records were found that match the specified search crieteria :( <a href=\"/search\"> See full table...</a></p>"}
 	a2.calculate_the_cells(data)
+	data[0][0]="Provinces that include '"+name.title() +"'"
 	return a2.template %{'title':"Searching Result",'navbar':navbar,'form':form,'table':a2.make_table(data)}
 
 def get_filter_criterias():
@@ -170,22 +170,38 @@ def get_filter_cond():
 			if i=='name':
 				inputs[i]="""	<div class="inputs">
 			<label>Province name includes</label>
-			<input size="45" type="search"  pattern=".{1,}" name="name" required ><br>
+			<input size="45" type="search" placeholder="Only English characters are allowed." pattern="[A-Za-z]{1,}" name="name" required ><br>
 			<label for="cs">Case-sensitive</label>
 			<input type="checkbox" name="cs" value="enabled">
 			<br>
 		</div>"""
 			else:
 				inputs[i]=form_input%{'ln':ln[i],'name':i}
-	return a2.template %{'title':"Set filter condition(s)",'navbar':navbar,'table':a2.make_table(a2.contents),'form':cond%inputs}
+	return a2.template %{'title':"Set Filter Condition(s)",'navbar':navbar,'table':a2.make_table(a2.contents),'form':cond%inputs}
+
 def filt_name(search,cs,data):
-	if cs=='enabled':
-		new_data=[row for row in data if search in row[0]]
-	else:
-		search=search.replace('İ','i').lower()
-		new_data=[row for row in data if search in row[0].replace('İ','i').lower()]
-	return new_data
+	new_data=[]
 	
+	if cs=='enabled':
+		for row in data:
+			tempdata=row[0]
+			for i in tr_to_en.keys():
+				tempdata=tempdata.replace(i,tr_to_en[i])
+			if search in tempdata:
+				new_data+=[row]
+	else:
+		for i in tr_to_en.keys():
+			search=search.replace(i,tr_to_en[i])
+		search=search.lower()
+		for row in data:
+			tempdata=row[0]
+			for i in tr_to_en.keys():
+				tempdata=tempdata.replace(i,tr_to_en[i])
+			tempdata=tempdata.lower()
+			if search in tempdata:
+				new_data+=[row]
+	return new_data
+
 def filter_it(col,val,gl,data):
 	if gl=='g':
 		new_data=[row for row in data if row[a2.find_starting_point(col,a2.contents)]>int(val)]
@@ -198,6 +214,7 @@ def filter_result():
 	meta,content=deepcopy(meta1),deepcopy(content1)
 	data=[]
 	data+=content
+	table_header={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
 	di={'name':"",'pop':"",'in':"",'out':"",'net':"",'rate':""}
 	for i in di.keys():
 		if type(request.POST.get(i)) is str:
@@ -208,16 +225,33 @@ def filter_result():
 				search=request.POST.get(i)
 				cs=request.POST.get('cs')
 				data=filt_name(search,cs,data)
+				if cs=="enabled":
+					cs="Yes"
+				else:
+					cs="No"
+				table_header[i]="""<div class="tooltip">%s,
+  <span class="tooltiptext">%s includes: '%s'<br>Case-sensitive: %s</span>
+</div>"""%(col_names[i],col_names[i],search,cs)
 			else:
 				col=i
 				val=request.POST.get(i)
 				gl=request.POST.get(i+"gl")
 				data=filter_it(col,val,gl,data)
+				if gl=="g":
+					gl="greater than"
+				else:
+					gl="less than"
+				table_header[i]="""<div class="tooltip">%s,
+  <span class="tooltiptext">%s is %s %s.</span>
+</div>"""%(col_names[i],col_names[i],gl,val)
+				
 	if len(data)==0:
-		return a2.template %{'title':"Filter Result",'navbar':navbar,'form':"",'table':"<p>No records were found that match the specified search crieteria :( <a href=\"/search\"> See full table...</a></p>"}
+		return a2.template %{'title':"Filter Result",'navbar':navbar,'form':"",'table':"<p>No records were found that match the specified search crieteria :( <a href=\"/filter\"> See full table...</a></p>"}
+	
+	meta[0][0]="Filtered by: %(name)s %(pop)s %(in)s %(out)s %(net)s %(rate)s"%table_header
 	data=meta+data
+	a2.calculate_the_cells(data)
 	return a2.template %{'title':"Filter Result",'navbar':navbar,'form':"",'table':a2.make_table(data)}
-
 def server_static(path):
 	return static_file(path, root=".")
 route('/search', 'GET', search_province)
